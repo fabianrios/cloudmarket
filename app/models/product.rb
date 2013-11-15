73bdb0e481a1475810255a1b15b8466337af4ba6
fabianrios <hola@fabianrios.co>
2013-11-15 02:41:27 -0500

    new ones

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :adress, :category, :highprice, :price, :quantity, :storeName, :title, :user_id
 
+  validates_presence_of :price, :quantity, :title, :storeName
+
   belongs_to :user
 end
