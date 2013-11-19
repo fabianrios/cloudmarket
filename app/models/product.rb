70d7820cecfbb85f961d3cc54f55f176b2d0bde9
fabianrios <hola@fabianrios.co>
2013-11-19 00:09:16 -0500

    presentacion

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
-  attr_accessible :adress, :category, :highprice, :price, :quantity, :storeName, :title, :user_id
+  attr_accessible :adress, :category, :highprice, :price, :quantity, :storeName, :title, :user_id, :unit
 
-  validates_presence_of :price, :quantity, :title, :storeName
+  validates_presence_of :price, :quantity, :title, :storeName, :unit
 
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131113074143) do
+ActiveRecord::Schema.define(:version => 20131119044829) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20131113074143) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+    t.string   "unit"
   end
 
   create_table "users", :force => true do |t|
